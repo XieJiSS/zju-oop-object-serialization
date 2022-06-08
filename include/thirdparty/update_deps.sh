1386ecf30869f4a78807f4f71b219263a4a66a93
XieJiSS <XieJiSS@users.noreply.github.com>
2022-06-09 01:42:57 +0800

    chore: update deps script

---
+++ include/thirdparty/update_deps.sh
@@ -0,0 +1,5 @@
+cd "$(dirname "$0")"
+wget -O- https://raw.githubusercontent.com/leethomason/tinyxml2/master/tinyxml2.h > tinyxml2.h
+wget -O- https://raw.githubusercontent.com/leethomason/tinyxml2/master/tinyxml2.cpp > tinyxml2.cpp
+wget -O- https://raw.githubusercontent.com/ReneNyffenegger/cpp-base64/master/base64.h > base64.h
+wget -O- https://raw.githubusercontent.com/ReneNyffenegger/cpp-base64/master/base64.cpp > base64.cpp
