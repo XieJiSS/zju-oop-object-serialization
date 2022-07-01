0b1e80c2eae15d8e989146853d1a45995e54a299
XieJiSS <XieJiSS@users.noreply.github.com>
2022-07-01 15:18:16 +0800

    chore: use correct arg format; also download license

--- include/thirdparty/update_deps.sh
+++ include/thirdparty/update_deps.sh
@@ -2,7 +2,9 @@
 
 cd "$(dirname "$0")"
 
-wget -O- https://raw.githubusercontent.com/leethomason/tinyxml2/master/tinyxml2.h > tinyxml2.h
-wget -O- https://raw.githubusercontent.com/leethomason/tinyxml2/master/tinyxml2.cpp > tinyxml2.cpp
-wget -O- https://raw.githubusercontent.com/ReneNyffenegger/cpp-base64/master/base64.h > base64.h
-wget -O- https://raw.githubusercontent.com/ReneNyffenegger/cpp-base64/master/base64.cpp > base64.cpp
+wget -O - https://raw.githubusercontent.com/leethomason/tinyxml2/master/tinyxml2.h > tinyxml2.h
+wget -O - https://raw.githubusercontent.com/leethomason/tinyxml2/master/tinyxml2.cpp > tinyxml2.cpp
+wget -O - https://raw.githubusercontent.com/leethomason/tinyxml2/master/LICENSE.txt > tinyxml2_LICENSE
+wget -O - https://raw.githubusercontent.com/ReneNyffenegger/cpp-base64/master/base64.h > base64.h
+wget -O - https://raw.githubusercontent.com/ReneNyffenegger/cpp-base64/master/base64.cpp > base64.cpp
+wget -O - https://raw.githubusercontent.com/ReneNyffenegger/cpp-base64/master/LICENSE > cpp-base64_LICENSE
